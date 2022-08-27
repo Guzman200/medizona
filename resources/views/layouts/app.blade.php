562da071e867139574d05875abe5ece0873f56b2
Pedro Guzman <my.rg.developer@gmail.com>
2022-08-26 22:30:36 -0500

    Delete carpet template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,37 +1,3 @@
-
-{{--<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    
-
-    <title>App - tareas</title>
-  
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	
-
-</head>
-<body class="layout-top-nav light-skin theme-primary fixed">
-	
-<div class="wrapper" id="app">
-
-    <app-component></app-component> 
-
-</div>
-<!-- ./wrapper -->
-	
-	
-    <script src="{{mix('js/app.js')}}"></script>
-
-	
-</body>
-</html>
---}}
-
 <!DOCTYPE html>
 <html lang="es">
 
